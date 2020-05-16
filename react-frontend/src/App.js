c335e03325af93d59d75f3891e29b2ad844d48b9
Sudeep <sudeep.srivastava@outlook.com>
2020-05-16 18:36:06 +0100

    Adding more docker projects

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Tuhm Bhi Bramhasmi!
+          Ahm Bramhasmi!
         </p>
       </header>
     </div>
